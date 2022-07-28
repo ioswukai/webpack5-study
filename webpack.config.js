0b3cd7f2002de77e7733d212065e491fb85b08e7
吴凯 <ioswukai@163.com>
2022-07-28 10:52:42 +0800

    处理 图片、字体图标、mp3等其他资源

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ module.exports = {
         // path.resolve()方法返回一个绝对路径
         // __dirname：模块化函数的参数，表示当前文件的文件夹目录
         path: path.resolve(__dirname, "dist"),
-        // 文件名
-        filename: "main.js"
+        // 入口文件打包输出文件名
+        filename: "static/js/main.js"
     },
     // 加载器
     module: {
@@ -50,6 +50,33 @@ module.exports = {
                     "css-loader",
                     "stylus-loader", // 将stylus编译成css文件
                 ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp|svg)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        // 小于10kb的图片转base64
+                        // 优点：减少请求数量  缺点：体积会更大
+                        maxSize: 10 * 1024, // 10kb
+                    },
+                },
+                generator: { // 修改打包输出资源的路径
+                    // 输出图片名称
+                    // 图片会有一个id，它会根据文件内容生成一个唯一的hash值, 目的是保证图片的名字不会冲突
+                    // [hash:10] hash值取前10位，减小文件名长度，打包后可减小 几个字节
+                    // [ext] 是文件扩展名，如果之前是.png，那么打包生成的文件就也是.png
+                    // [query] 携带的参数，如果url地址写了想?xx=xxx的查询参数，那么该参数也会显示在文件名里
+                    filename: "static/images/[hash:10][ext][query]",
+                },
+            },
+            {
+                test: /\.(ttf|woff2?|map3|map4|avi)$/,
+                type: "asset/resource",
+                generator: {
+                    // 输出名称
+                    filename: "static/media/[hash:10][ext][query]",
+                },
             },
         ]
     },
